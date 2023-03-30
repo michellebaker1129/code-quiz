29c49d79cb0c7b732d302221476a888e4378d767
Michelle <michellebaker1129@gmail.com>
2023-03-30 07:55:03 -0700

    build out initial html frame

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+let questions= [
+    {
+        question: "Which of the following is NOT a JavaScript keyword?",
+        answers: ["var", "let", "const", "function"],
+        correctAnswer: "var",
+    }, {
+        question: "Which of the following is NOT a JavaScript keyword?",
+        answers: ["var", "let", "const", "function"],
+        correctAnswer: "var",
+    }, {
+        question: "Which is the highest heading element?",
+        answers: ["h1", "h2", "h3", "h4"],
+        correctAnswer: "h1",   
+    }, {
+        question: "Which is the lowest heading element?",
+        answers: ["h1", "h2", "h3", "h4"],
+        correctAnswer: "h4",
+    }
+]
+// Select start button
+// Add event listener to start button
+// Hide intro section
+// Reveal quiz section 
+// Load first question
+// Check answer for correct
+// If correct increase score
+// Else decrease timer (there is a whole side thing here, a timer needs to exist to begin with)
+// Repeat until out of questoins
+// Hide quiz section 
+// Reveal end section 
+// Get user's initials
+// Save user's initials and score to local storage
+
+// What is required for each step above?
+// Am I going to do this once or more than once?
+//  If more than once, loop or create a function to call in several places
