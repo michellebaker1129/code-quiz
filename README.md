83181bcf17b5e3fc85b0fca7bbffc6a188edfe22
Michelle <michellebaker1129@gmail.com>
2023-04-01 20:04:00 -0700

    break down into pseudocode, add pseudocode steps to existing files.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # code-quiz
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+## Todo
+
+- Create a object to handle high scores
+  - Read from localStorage on page load to see if any existing scores
+  - update localStorage with current user score
+  - Prompt the user to enter initials and store into localStorage
+- Create a object that updates the DOM with which stage of the quiz we're on (intro, quiz, end)
+- Create a object that handles quiz questions
+  - Replace the prev question markup with current question markup
+  - Handle answers with correct or incorrect/notify the user
+  - Handle updating score
+  - Handle timer
+  - Build out the markup for individual questions
+
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,55 @@
-let questions= [
-    {
-        question: "Which of the following is NOT a JavaScript keyword?",
-        answers: ["var", "let", "const", "function"],
-        correctAnswer: "var",
-    }, {
-        question: "Which of the following is NOT a JavaScript keyword?",
-        answers: ["var", "let", "const", "function"],
-        correctAnswer: "var",
-    }, {
-        question: "Which is the highest heading element?",
-        answers: ["h1", "h2", "h3", "h4"],
-        correctAnswer: "h1",   
-    }, {
-        question: "Which is the lowest heading element?",
-        answers: ["h1", "h2", "h3", "h4"],
-        correctAnswer: "h4",
-    }
-]
+const questionsObject = {
+    // add property to keep track of currently shown question
+    currentQuestion: 0,
+    questions: [
+        {
+            id: 0,
+            question: "Which of the following is NOT a JavaScript keyword?",
+            answers: ["var", "let", "const", "give"],
+            correctAnswer: "give",
+        },  {
+            id: 1,
+            question: "Which is the highest heading element?",
+            answers: ["h1", "h2", "h3", "h4"],
+            correctAnswer: "h1",   
+        }, {
+            id: 2,
+            question: "Which is the lowest heading element?",
+            answers: ["h1", "h2", "h3", "h4"],
+            correctAnswer: "h4",
+        }
+    ],
+    answerQuestion: function (question, answerGiven) {
+        // compare answerGiven to question.correctAnswer
+        // return true or false
+    },
+    // add method to show next question
+    showNextQuestion: function () {},
+
+}
+
+const questionMarkup = `<div class="question">
+    <h2>${obj.questions[0].question}</h2>
+    <ul>
+        <li>
+            <button>
+                ${questions[0].answers[0]}
+            </button>
+        </li>
+        <li>${questions[0].answers[1]}</li>
+        <li>${questions[0].answers[2]}</li>
+        <li>${questions[0].answers[3]}</li>
+    </ul>
+</div>
+`;
+
 // Select start button
 // Add event listener to start button
 // Hide intro section
 // Reveal quiz section 
 // Load first question
 // Check answer for correct
-// If correct increase score
+// If correct, increase score
 // Else decrease timer (there is a whole side thing here, a timer needs to exist to begin with)
 // Repeat until out of questoins
 // Hide quiz section 
